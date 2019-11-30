0508186941d7e0767e723ace3267382828e91092
Creeper Lv <1376021184@qq.com>
2019-11-30 23:38:15 +0800

    Added more commands to SimpleBlogModule.

--- LWSwnS/SimpleBlogModule/ShellCommands.cs
+++ LWSwnS/SimpleBlogModule/ShellCommands.cs
@@ -15,7 +15,7 @@ namespace SimpleBlogModule
         {
             ModuleDescription moduleDescription = new ModuleDescription();
             moduleDescription.Name = "SimpleBlog-ShellCommands";
-            moduleDescription.version = new Version(0, 0, 1, 0);
+            moduleDescription.version = new Version(0, 0, 2, 0);
             CommandHandler.RegisterCommand("SimpleBlog:Add-Post", (string fileName, object content, StreamWriter writer) =>
             {
                 string FilePath = Path.Combine(new DirectoryInfo("./Padth").FullName, fileName);
@@ -24,6 +24,36 @@ namespace SimpleBlogModule
                 }
                 File.WriteAllText(FilePath, (string)content);
                 ShellFeedbackData shellFeedbackData = new ShellFeedbackData();
+                shellFeedbackData.writer = writer;
+                shellFeedbackData.SendBack();
+                return true;
+            });
+            CommandHandler.RegisterCommand("SimpleBlog:Del-Post", (string fileName, object content, StreamWriter writer) =>
+            {
+                string FilePath = Path.Combine(new DirectoryInfo("./Padth").FullName, fileName);
+                if (File.Exists(FilePath)) {
+                    File.Delete(FilePath);
+                }
+
+                ShellFeedbackData shellFeedbackData = new ShellFeedbackData();
+                shellFeedbackData.writer = writer;
+                shellFeedbackData.SendBack();
+                return true;
+            });
+            CommandHandler.RegisterCommand("SimpleBlog:Get-Post", (string fileName, object content, StreamWriter writer) =>
+            {
+                string FilePath = Path.Combine(new DirectoryInfo("./Padth").FullName, fileName);
+                
+
+                ShellFeedbackData shellFeedbackData = new ShellFeedbackData();
+                if (File.Exists(FilePath))
+                {
+                    shellFeedbackData.DataBody = File.ReadAllText(FilePath);
+                }
+                else
+                {
+                    shellFeedbackData.StatusLine="FILE_NO_FOUND";
+                }
                 shellFeedbackData.writer = writer;
                 shellFeedbackData.SendBack();
                 return true;
