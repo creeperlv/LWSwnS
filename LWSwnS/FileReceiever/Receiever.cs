d83fd365c0487b97baed8aa7ceae4bbc5a518a83
Creeper Lv <1376021184@qq.com>
2019-12-03 23:36:18 +0800

    Preparing FileReceiverModule.

---
+++ LWSwnS/FileReceiever/Receiever.cs
@@ -0,0 +1,17 @@
+﻿using LWSwnS.Api.Modules;
+using System;
+
+namespace FileReceiever
+{
+    public class Receiever : ExtModule
+    {
+        public static Version ModuleVersion = new Version(0, 0, 1, 0);
+        public ModuleDescription InitModule()
+        {
+            ModuleDescription moduleDescription = new ModuleDescription();
+            moduleDescription.Name = "FileReceieverModule";
+            moduleDescription.version = ModuleVersion;
+            return moduleDescription;
+        }
+    }
+}
