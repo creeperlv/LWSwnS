0a0abe8bc8213fbeb2e57740fe48b15a8ca48716
Creeper Lv <1376021184@qq.com>
2019-12-06 22:19:46 +0800

    Code Embeded Page is now working! Example is in Test.CEPTest!

--- LWSwnS/LWSwnS.WebPage/CodeEmbededPage.cs
+++ LWSwnS/LWSwnS.WebPage/CodeEmbededPage.cs
@@ -1,16 +1,90 @@
-﻿using System;
+﻿using Microsoft.CodeAnalysis.CSharp.Scripting;
+using Microsoft.CodeAnalysis.Scripting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace LWSwnS.WebPage
 {
     public class CodeEmbededPage
     {
-        string File;
-        CodeEmbededPage(string pathToFile)
+        string PageFile;
+        public CodeEmbededPage(string pathToFile)
+        {
+            PageFile = pathToFile;
+        }
+        public async Task<string> ExecuteAndRetire()
+        {
+            //CSharpScript.
+            var c = Resolve();
+            string content = "";
+            ScriptState state = null;
+            foreach (var item in c)
+            {
+                if (item.type == 0)
+                {
+                    content += item.content;
+                }
+                else if (item.type == 1)
+                {
+                    if (state == null)
+                        state = await CSharpScript.RunAsync(item.content);
+                    else await state.ContinueWithAsync(item.content);
+                    
+                    content += state.ReturnValue;
+                }
+                else if (item.type == 2)
+                {
+                    foreach (var varible in state.Variables)
+                    {
+                        if (varible.Name == item.content)
+                        {
+                            content += varible.Value;
+                            break;
+                        }
+                    }
+                }
+            }
+            return content;
+        }
+        //Untested method...
+        [Serializable]
+        public struct CEPItem
+        {
+            public string content;
+            public int type;
+        }
+        public List<CEPItem> Resolve()
         {
 
-        }
-        string Resolve() {
-            return "";
+            var p = File.ReadAllText(PageFile);
+            List<CEPItem> pageContent = new List<CEPItem> ();
+            while (p.IndexOf("[CODE:") > 0|| p.IndexOf("[VALUE:") > 0)
+            {
+
+                if (p.IndexOf("[CODE:") < p.IndexOf("[VALUE:")&&p.IndexOf("[CODE:")>0)
+                {
+
+                    pageContent.Add(new CEPItem() { type = 0, content = p.Substring(0, p.IndexOf("[CODE:")) });
+                    p = p.Substring(p.IndexOf("[CODE:") + "[CODE:".Length);
+                    pageContent.Add(new CEPItem() { type = 1, content = p.Substring(0, p.IndexOf(":CODE]")) });
+                    p = p.Substring(p.IndexOf(":CODE]") + ":CODE]".Length);
+                }
+                else
+                {
+
+                    pageContent.Add(new CEPItem() { type = 0, content = p.Substring(0, p.IndexOf("[VALUE:")) });
+                    p = p.Substring(p.IndexOf("[VALUE:") + "[VALUE:".Length);
+                    pageContent.Add(new CEPItem() { type = 2, content = p.Substring(0, p.IndexOf(":VALUE]")) });
+                    p = p.Substring(p.IndexOf(":VALUE]") + ":VALUE]".Length);
+                }
+            }
+            if (p.Length > 0)
+            {
+                pageContent.Add(new CEPItem() { type = 0, content = p });
+            }
+            return pageContent;
         }
     }
 }
---
+++ LWSwnS/Tests.CEPTest/Program.cs
@@ -0,0 +1,25 @@
+﻿using LWSwnS.WebPage;
+using System;
+
+namespace Tests.CEPTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("CEP - Code Embeded Page");
+            Console.WriteLine("Please specify a page:");
+            string cep=Console.ReadLine();
+            CodeEmbededPage codeEmbededPage = new CodeEmbededPage(cep);
+
+            var pc = codeEmbededPage.Resolve();
+            foreach (var item in pc)
+            {
+                Console.WriteLine((item.type==0?"[HTML]":"[C#]")+item.content);
+            }
+            var a=codeEmbededPage.ExecuteAndRetire();
+            a.Wait();
+            Console.WriteLine(a.Result);
+        }
+    }
+}
