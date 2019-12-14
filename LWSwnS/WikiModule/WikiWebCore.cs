4c7e81ab38b3231835473be0771504327f66afbc
Creeper Lv <1376021184@qq.com>
2019-12-14 14:22:11 +0800

    Doing a wiki module.

---
+++ LWSwnS/WikiModule/WikiWebCore.cs
@@ -0,0 +1,47 @@
+﻿using LWSwnS.Api.Modules;
+using LWSwnS.Api.Web;
+using LWSwnS.Configuration;
+using LWSwnS.Core.Data;
+using LWSwnS.Diagnostic;
+using System;
+using System.Threading.Tasks;
+
+namespace WikiModule
+{
+    public class WikiWebCore : ExtModule
+    {
+        string PageTemplate = "";
+        UniversalConfiguration config = new UniversalConfiguration();
+        static Version ModuleVersion = new Version(0,0,1,0);
+        public ModuleDescription InitModule()
+        {
+            Task.Run(async () => {
+                Debugger.currentDebugger.Log("Automatic Configuration Reload task initialized.");
+                while (true)
+                {
+                    Load();
+                    await Task.Delay(5000);
+                }
+            });
+            WebServer.AddIgnoreUrlPrefix("/wiki");
+            EventHandler<HttpRequestData> eventHandler = (a, b) => {
+                if (b.requestUrl.ToUpper().StartsWith("/WIKI"))
+                {
+
+                }
+            };
+            WebServer.AddHttpRequestHandler(eventHandler);
+            {
+                ModuleDescription description = new ModuleDescription();
+                description.Name = "Wiki-Module-Web";
+                description.version = ModuleVersion;
+                return description;
+            }
+        }
+        void Load()
+        {
+            config = UniversalConfigurationLoader.LoadFromFile("./Configs/WikiModule.ini");
+
+        }
+    }
+}
