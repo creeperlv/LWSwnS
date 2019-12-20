18a55260852fec90597c03f166200dc85e2535e7
Creeper Lv <1376021184@qq.com>
2019-12-20 21:29:01 +0800

    Added an experimental script for linux to 'install' LWSwnS.

---
+++ LWSwnS/LinuxInstall.sh
@@ -0,0 +1,20 @@
+echo '#######################################'
+echo '#Experimental Install Script for Linux#'
+echo '#######################################'
+#if [ `whoami` = "root" ];then
+#	echo "You are in sudo"
+#
+#else
+#	echo "Please run in sudo"
+#fi
+command -v dotnet >/dev/null 2>&1 || { echo >&2 "This application relies on .NET Core 3.0 or newer."; exit 1;}
+mkdir temp
+cd temp
+git clone https://github.com/creeperlv/LWSwnS.git
+cd LWSwnS/LWSwnS
+dotnet build
+cd ..
+cd ..
+cd ..
+mv -f temp/LWSwnS/LWSwnS/LWSwnS/bin/netcoreapp3.0/ ./LWSwnS/
+rm -rf temp/LWSwnS
