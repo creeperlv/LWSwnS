b609fd5abe331106cafa147a16816e06a9a6aaf1
Creeper Lv <1376021184@qq.com>
2019-12-30 15:23:40 +0800

    PSModule: NativePS options can be operated in local shell directly now. Lengthen the time before destorying the execuation result and it can be configured with the configuration 'DestoryResultDelay'.

--- LWSwnS/PowerShellModule/ShellCore.cs
+++ LWSwnS/PowerShellModule/ShellCore.cs
@@ -1,6 +1,7 @@
 ﻿using LWSwnS.Api.Data;
 using LWSwnS.Api.Modules;
 using LWSwnS.Api.Shell;
+using LWSwnS.Api.Shell.Local;
 using LWSwnS.Api.Web;
 using LWSwnS.Configuration;
 using Microsoft.CodeAnalysis.FlowAnalysis;
@@ -17,7 +18,7 @@ namespace PowerShellModule
 {
     public class ShellCore : ExtModule
     {
-        public static Version ShellVersion=new Version(0, 1, 3, 0);
+        public static Version ShellVersion = new Version(0, 1, 3, 0);
         public ModuleDescription InitModule()
         {
             ModuleDescription moduleDescription = new ModuleDescription();
@@ -25,6 +26,42 @@ namespace PowerShellModule
             Tasks.RegisterTask(Load, Tasks.TaskType.Every30Seconds);
             moduleDescription.Name = "PowerShellModule-Shell";
             moduleDescription.version = ShellVersion;
+            LocalShell.Register("SetNativePS", (s) =>
+            {
+                try
+                {
+                    bool b = bool.Parse(s);
+                    if (VariablesPool.config.ContainsKey("UseNativePS")) VariablesPool.config["UseNativePS"] = b + ""; else VariablesPool.config.Add("UseNativePS", b + "");
+                    VariablesPool.config.SaveToFile("./Configs/psmodule.ini");
+                }
+                catch (Exception)
+                {
+                }
+            });
+            LocalShell.Register("SetNativePreviewPS", (s) =>
+            {
+                try
+                {
+                    bool b = bool.Parse(s);
+                    if (VariablesPool.config.ContainsKey("UseNativePreviewPS")) VariablesPool.config["UseNativePreviewPS"] = b + ""; else VariablesPool.config.Add("UseNativePreviewPS", b + "");
+                    VariablesPool.config.SaveToFile("./Configs/psmodule.ini");
+                }
+                catch (Exception)
+                {
+                }
+            });
+            LocalShell.Register("SetDestoryDelay", (s) =>
+            {
+                try
+                {
+                    int d = int.Parse(s);
+                    if (VariablesPool.config.ContainsKey("DestoryResultDelay")) VariablesPool.config["DestoryResultDelay"] = d + ""; else VariablesPool.config.Add("DestoryResultDelay", d + "");
+                    VariablesPool.config.SaveToFile("./Configs/psmodule.ini");
+                }
+                catch (Exception)
+                {
+                }
+            });
             CommandHandler.RegisterCommand("powershell-execute", PSE);
             CommandHandler.RegisterCommand("ps-e", PSE);
             CommandHandler.RegisterCommand("powershell-add-script", AddScript);
@@ -85,7 +122,7 @@ namespace PowerShellModule
             c.BaseStream.WriteTimeout = 30000;
             Task.Run(async () =>
             {
-                LWSwnS.Diagnostic.Debugger.currentDebugger.Log("Starting Script:"+a);
+                LWSwnS.Diagnostic.Debugger.currentDebugger.Log("Starting Script:" + a);
                 try
                 {
 
@@ -97,7 +134,15 @@ namespace PowerShellModule
                     LWSwnS.Diagnostic.Debugger.currentDebugger.Log(e.Message, LWSwnS.Diagnostic.MessageType.Error);
                 }
                 LWSwnS.Diagnostic.Debugger.currentDebugger.Log("Invoke completed");
-                await Task.Delay(3000);
+                int Delay = 15000;
+                try
+                {
+                    Delay = int.Parse(VariablesPool.config.Get("DestoryResultDelay", "15000"));
+                }
+                catch (Exception)
+                {
+                }
+                await Task.Delay(Delay);
                 VariablesPool.PSInstances.Remove(pSInstance);
             }).Wait();
             ShellFeedbackData shellFeedbackData = new ShellFeedbackData();
@@ -182,15 +227,15 @@ namespace PowerShellModule
 
                     var scriptHome = VariablesPool.config.Get("ScriptHome", "./PSScripts/");
                     var scriptF = Path.Combine(scriptHome, script);
-                    startInfo.Arguments = scriptF+" "+parameter;
+                    startInfo.Arguments = scriptF + " " + parameter;
                     startInfo.WorkingDirectory = Environment.CurrentDirectory;
                     startInfo.CreateNoWindow = true;
                     startInfo.RedirectStandardOutput = true;
-                    var p=Process.Start(startInfo);
+                    var p = Process.Start(startInfo);
                     string s;
                     while (p.HasExited)
                     {
-                        if((s = p.StandardOutput.ReadLine()) != null)
+                        if ((s = p.StandardOutput.ReadLine()) != null)
                         {
 
                             if (Result == "")
