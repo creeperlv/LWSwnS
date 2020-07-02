76c32e4abb2dabde3e04e2b7376b9881f814ab90
Creeper Lv <creeperlv38@outlook.com>
2020-07-03 01:36:00 +0800

    Making a new way for extensions to handle http response.

---
+++ LWSwnS/LWSwnS.Core/Pipeline/HttpPipelineData.cs
@@ -0,0 +1,12 @@
+﻿using CLUNL.Pipeline;
+using LWSwnS.Core.Data;
+
+namespace LWSwnS.Core.Pipeline
+{
+    public class HttpPipelineData : PipelineData
+    {
+        public HttpPipelineData(object PrimaryData, object SecondaryData, HttpRequestData Options) : base(PrimaryData, SecondaryData, Options)
+        {
+        }
+    }
+}
---
+++ LWSwnS/LWSwnS.Core/Pipeline/HttpResponsePipelineProcessor.cs
@@ -0,0 +1,40 @@
+﻿using CLUNL.Pipeline;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LWSwnS.Core.Pipeline
+{
+    public class HttpResponsePipelineProcessor : IPipelineProcessor
+    {
+        List<IPipedProcessUnit> pipedProcessUnits = new List<IPipedProcessUnit>();
+        public void Init()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Init(ProcessUnitManifest manifest)
+        {
+            pipedProcessUnits=manifest.GetUnitInstances();
+        }
+
+        public PipelineData Process(PipelineData Input, bool IgnoreError)
+        {
+            //HttpPipelineData
+            foreach (var item in pipedProcessUnits)
+            {
+                var outdata=item.Process(Input);
+                if (!outdata.CheckContinuity(Input))
+                {
+                    throw new PipelineDataContinuityException(item);
+                }
+            }
+            return null;
+        }
+
+        public PipelineData Process(PipelineData Input)
+        {
+            return Process(Input, false);
+        }
+    }
+}
